f55c23aaa97a10ee31b3fc56626ef80164468940
Gil <gil.kovshi@gmail.com>
2022-01-08 21:52:43 +0200

    add Doxygen file

---
+++ exam/exam/Header.h
@@ -0,0 +1,17 @@
+#pragma once
+/**
+* @file Header.h
+* @brief header file for 1.4 excersize
+*/
+
+
+/**
+* @brief calculates the n th fibonacy number
+* @param elem - the index of the fibonacy number to calc
+* @author Gil Kovshi
+* @return the "elem" fibonacy number
+*/
+int sh()
+{
+
+}
--- exam/exam/Main.c
+++ exam/exam/Main.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
+/**
+* @file Main.c
+* @brief header file for 1.4 excersize
+*/
+
+
+/**
+* @brief prints a diamond made from '*'
+* @param number an unsigned odd number specifies the diamond's base length.
+* @author Gil Kovshi
+*/
 int main()
 {
-	 
+    int i;;
+    int j = 0;
+    int k = 0;
 }
